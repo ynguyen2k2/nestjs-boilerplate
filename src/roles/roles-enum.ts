63aca9c3543a447eea310161d1540cf9c9705cf5
ynguyen2024tt <ynguyen2024tt@gmail.com>
2025-12-17 14:25:53 +0700

    chore: update role enum

--- src/roles/roles-enum.ts
+++ src/roles/roles-enum.ts
@@ -1,7 +1,7 @@
 export const ROLES_KEY = 'roles'
 export enum RoleEnum {
-  ADMIN = 'admin',
-  USER = 'user',
-  MODERATOR = 'moderator',
+  ADMIN = 1,
+  USER = 2,
+  MODERATOR = 3,
 }
 export type ValidRole = RoleEnum.ADMIN | RoleEnum.USER | RoleEnum.MODERATOR
