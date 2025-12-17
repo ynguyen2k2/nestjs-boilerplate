56554b36b7cd57f000ced22875c83126f73d871d
ynguyen2024tt <ynguyen2024tt@gmail.com>
2025-12-17 15:43:31 +0700

    feat: add user seed

--- src/database/seeds/relational/user/user-seed.module.ts
+++ src/database/seeds/relational/user/user-seed.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
-
-import { UserSeedService } from './user-seed.service'
+import { UserSeedService } from '~/database/seeds/relational/user/user-seed.service'
 import { UserEntity } from '~/user/infrastructure/persistence/relational/entities/user-entity'
 
 @Module({
