1ce880dadfcba509343f2585dbf6f1b20cbdfdb4
ynguyen2024tt <ynguyen2024tt@gmail.com>
2025-12-18 14:00:17 +0700

    chore: modified eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,6 @@
 import eslint from '@eslint/js'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import globals from 'globals'
-import { off } from 'process'
-import { SemicolonPreference } from 'typescript'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
