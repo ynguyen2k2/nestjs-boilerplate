2e411302aff9d71069659735c4905bae1c2e1723
ynguyen2024tt <ynguyen2024tt@gmail.com>
2025-12-19 16:27:55 +0700

    feat: add auth module

--- src/auth/auth-providers.enum.ts
+++ src/auth/auth-providers.enum.ts
@@ -1,6 +1,6 @@
 export enum AuthProvidersEnum {
-  email = 'email',
-  facebook = 'facebook',
-  google = 'google',
-  apple = 'apple',
+  EMAIL = 'email',
+  FACEBOOK = 'facebook',
+  GOOGLE = 'google',
+  APPLE = 'apple',
 }
