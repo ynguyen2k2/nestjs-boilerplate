a9c0096d4006313f5f580d7037ddf6fa02919e15
ynguyen2024tt <ynguyen2024tt@gmail.com>
2025-12-24 16:30:12 +0700

    feat(config): enable AuthConfig in AllConfigType definition

--- src/config/config.type.ts
+++ src/config/config.type.ts
@@ -1,10 +1,11 @@
 import { AppConfig } from './app-config.type'
 import { DatabaseConfig } from '../database/config/database-config.type'
 import { FileConfig } from '~/files/config/file-config.type'
+import { AuthConfig } from '~/auth/config/auth-config.type'
 // import { AuthConfig } from '../auth/config/auth-config.type'
 export type AllConfigType = {
   app: AppConfig
   database: DatabaseConfig
   file: FileConfig
-  // auth: AuthConfig
+  auth: AuthConfig
 }
