bd24bf224ef8b5e1c8e1da0616f7ea5897520b84
ynguyen2024tt <ynguyen2024tt@gmail.com>
2025-12-30 16:34:08 +0700

    feat(migration): create initial database tables and relationships

--- src/database/migrations/1767074270442-inittable.ts
+++ src/database/migrations/1767074270442-inittable.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
 
-export class Inittable1767021896917 implements MigrationInterface {
-  name = 'Inittable1767021896917'
+export class Inittable1767074270442 implements MigrationInterface {
+  name = 'Inittable1767074270442'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
